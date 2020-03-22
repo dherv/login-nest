14d5fc2cc4a0565babc879bf5688d8a98ca7be89
dherv <damien.herv@gmail.com>
2020-03-22 10:17:00 +0900

    refactor(user): replace userId by id

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -22,9 +22,10 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+    const payload = { username: user.username, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
+      id: user.id,
     };
   }
 }
